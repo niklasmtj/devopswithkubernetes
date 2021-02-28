02068a296ca90513dd0b0a10be3fd10f8a3ae009
Niklas Metje <22395665+niklasmtj@users.noreply.github.com>
2021-02-28 14:51:43 +0100

    Exercise 1.07
    * Added current status endpoint
    * Added ingress and service definition
    * Updated deployment

--- main-app/app/index.js
+++ main-app/app/index.js
@@ -1,11 +1,23 @@
 import { v4 } from "uuid";
+import express from 'express';
+import path from 'path'
 
+const PORT = 3000;
+const app = express();
 const randomString = v4();
 
-const printString = () => {
-  console.log(`${new Date().toISOString()}: ${randomString}`)
-}
+let timeStamp;
 
-setInterval(() => {
+const printString = () => {
+  timeStamp = new Date().toISOString();
+  console.log(`${timeStamp}: ${randomString}`)
+}
+app.get('/', (req, res) => res.send(`${timeStamp}: ${randomString}`));
+
+const printingInterval = setInterval(() => {
   printString()
 }, 5000);
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
