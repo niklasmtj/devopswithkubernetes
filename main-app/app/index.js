d36d4af1f518f3ed179747737e8785929b677d13
Niklas Metje <22395665+niklasmtj@users.noreply.github.com>
2021-03-04 16:52:02 +0100

    Exercise 1.11
    * Updated Node Dockerfiles to not use node user
    * Updated main-app-server to read files from volume
    * Changed images to use latest tag for easier redeployments

--- main-app/app/index.js
+++ main-app/app/index.js
@@ -1,7 +1,7 @@
 import { v4 } from "uuid";
 import express from 'express';
 import dotenv from 'dotenv';
-import { readFile } from 'fs';
+import { readFile } from 'fs/promises';
 import path from 'path';
 
 dotenv.config()
@@ -11,22 +11,62 @@ const app = express();
 const randomString = v4();
 
 const TIMESTAMP_PATH = path.join("/", "app", "files");
+// const TIMESTAMP_PATH = process.env.NODE_ENV ? path.join("/", "app", "files") : process.cwd();
 
 let timeStamp;
+let pingPong;
 
-const printString = () => {
-  readFile(`${TIMESTAMP_PATH}/timestamp.txt`, {encoding: "utf-8"}, (err, data) => {
+const readTimestamp = async () => {
+  try {
+    const data = await readFile(`${TIMESTAMP_PATH}/timestamp.txt`, { encoding: "utf-8" })
+    return data;
+  } catch (err) {
     if (err) {
       console.error(err.message);
+      return "Error happened";
     }
-    timeStamp = data;
-    console.log(`${timeStamp}: ${randomString}`)
-  })
+  }
 }
-app.get('/', (req, res) => res.send(`${timeStamp}: ${randomString}`));
 
-const printingInterval = setInterval(() => {
-  printString()
+const readPingPongFile = async () => {
+  try {
+    const data = await readFile(`${TIMESTAMP_PATH}/ping-pong.txt`, { encoding: "utf-8" })
+    return parseInt(data, 10);
+  } catch (err) {
+    if (err) {
+      console.error(err.message);
+      return "Error happened";
+    }
+  }
+}
+
+const getStringValues = () => {
+  timeStamp = readTimestamp();
+  pingPong = readPingPongFile();
+}
+
+getStringValues();
+
+const buildString = (ts, pg) => {
+  return `
+  ${ts}: ${randomString}
+  Ping / Pong: ${pg}
+  `
+}
+
+app.get('/', async (req, res) => {
+  const t = await readTimestamp();
+  const p = await readPingPongFile();
+  const s = buildString(t, p)
+  res.send(s);
+});
+
+const printingInterval = setInterval(async () => {
+  // getStringValues()
+  const t = await readTimestamp();
+  const p = await readPingPongFile();
+  const s = buildString(t, p)
+  console.log(s);
 }, 5000);
 
 app.listen(PORT, () => {
--- ping-pong/index.js
+++ ping-pong/index.js
@@ -1,11 +1,30 @@
 import express from 'express';
+import {writeFile, readFile} from 'fs';
+import path from 'path'
 
 const PORT = 3000;
 const app = express();
 
-let pingCounter = 0;
+const TIMESTAMP_PATH = path.join("/", "app", "files");
+// const TIMESTAMP_PATH = process.env.NODE_ENV ? path.join("/", "app", "files") : process.cwd();
 
-app.get('/', (req, res) => res.send(`pong ${pingCounter++}`));
+const readStartingCount = () => {
+  readFile(`${TIMESTAMP_PATH}/ping-pong.txt`, {encoding: "utf-8"}, (err, data) => {
+    if (err) return 0;
+    return data;
+  })
+  return 0;
+}
+
+let pingCounter = readStartingCount();
+
+app.get('/', (req, res) => {
+  pingCounter++
+  writeFile(`${TIMESTAMP_PATH}/ping-pong.txt`, pingCounter.toString(), {encoding: 'utf-8'}, (err) => {
+    if (err) console.error(err.message);
+  })
+  res.send(`pong ${pingCounter}`)
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
